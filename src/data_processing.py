b92f356b0983283260458769f7b6a359193df037
adrianlopezlanchares <202107370@alu.comillas.edu>
2025-06-24 21:51:25 +0200

    load .csv files into windows

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,6 +1,32 @@
 import pandas as pd
 import os
 import re
+import numpy as np
+from typing import Dict, List, Tuple
+
+COLUMNS_PROCESSED = [
+    "in.bedrooms_processed",  # int
+    "in.duct_leakage_processed",
+    "in.duct_insulation_processed",  # Separate into leakage / insulation: X% / (Uniinsulated/R-n)
+    "in.duct_location_processed",  # Places of the house
+    "in.geometry_floor_area_processed",  # 1000-1499, ...
+    "in.geometry_stories_processed",  # int
+    "in.geometry_wall_type_processed",  # Materials
+    "in.ground_thermal_conductivity_processed",  # float
+    "in.hvac_has_ducts_processed",  # Yes / No
+    "in.insulation_ceiling_processed",  # R-n / Uninsulated
+    "in.insulation_floor_processed",  # R-n / Uninsulated
+    "in.insulation_foundation_wall_processed",  # Wall R-n, Exterior
+    "in.insulation_roof_processed",  # Finished / Unfinished, R-n
+    "in.insulation_wall_processed",  # Material, Uninsulated / R-n
+    "in.occupants_processed",  # int
+    "in.orientation_processed",  # North, Northwest...
+    "in.roof_material_processed",  # Material
+    "in.sqft_processed",  # int
+    "in.windows_processed",  # Single/Double/Triple, Low-E/Clear, Metal/Non-metal, Air/Exterior Clear Storm, (L/M-Gain)
+    "in.window_areas_processed",
+    "in.vintage_processed",
+]
 
 
 def get_cols(building_data: pd.DataFrame, building_id: int) -> str:
@@ -166,6 +192,32 @@ def merge_building_data() -> pd.DataFrame:
             )
 
     return all_buildings_data
+
+
+def create_windows(
+    timeseries: pd.DataFrame, resstock: pd.DataFrame, window_size: int = 24
+) -> Tuple[np.ndarray, np.ndarray]:
+    X, y = [], []
+    for i in range(window_size, len(timeseries)):
+        print(f"{i}/{len(timeseries)}               ", end="\r")
+
+        bldg_id = timeseries.iloc[i]["bldg_id"]
+        if timeseries.iloc[i - window_size]["bldg_id"] != bldg_id:
+            continue
+
+        static_features = resstock.loc[bldg_id].values
+
+        ts_window = timeseries[i - window_size : i][
+            ["indoor_temp", "consumption"]
+        ].values
+
+        target = timeseries.iloc[i]["indoor_temp"]
+        features = np.concatenate([ts_window.flatten(), static_features])
+
+        X.append(features)
+        y.append(target)
+
+    return np.array(X), np.array(y)
 
 
 def process_resstock_data(df: pd.DataFrame) -> pd.DataFrame:
